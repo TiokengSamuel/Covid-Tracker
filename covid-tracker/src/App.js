fb278e7cc1a861e4cd0cef0256f9e22821684423
TiokengSamuel <samueledward40@gmail.com>
2021-04-15 00:55:02 +0100

    setting up the startaer app and api

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core"
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA', 'UK'
+  ]);
+
+  useEffect(() => {
+
+    const getCountriesData = async () => (
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ));
+      }))
+     
+  }, [])
+
   return (
     <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+          variant="outlined"
+          value="abc"
+          >
+            {
+              countries.map((country) => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
       
     </div>
   );
