c75528dc9278d910713969504539c0d6994ec565
TiokengSamuel <samueledward40@gmail.com>
2021-04-18 20:45:11 +0100

    covid

--- covid-tracker/src/LineGraph.js
+++ covid-tracker/src/LineGraph.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+// import { Line } from "react-chartjs-2";
 
 function LineGraph() {
-    return (
-        <div className="lineGraph">
-            
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then(response => response.json())
+        .then(data => {
+
+        })
+    }, []); 
+
+    return ( 
+        <div>
+            <h1>Im a  graph</h1>
+            {/* <Line
+                data
+                options
+            /> */}
+
         </div>
     )
 }
